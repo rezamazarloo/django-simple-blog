dce7b31310bf0ad9f17ab65ad56939faefc9c192
reza <rezamazarloo@yahoo.com>
2023-02-28 15:18:29 +0330

    added combination of publish + post slug

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,11 +8,18 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    # post = get_object_or_404(Post, status=Post.Status.PUBLISHED, id=id)
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No post found!!")
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No post found!!")
 
     return render(request, "blog/post/detail.html", {"post": post})
