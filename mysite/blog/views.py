f628a6abb7dd0d727db6fb297b2d548fd16c55f8
reza <rezamazarloo@yahoo.com>
2023-03-04 18:59:18 +0330

    added comment to blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 # def post_list(request):
@@ -37,12 +38,14 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No post found!!")
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(request, "blog/post/detail.html", {"post": post, "form":form, "comments":comments})
 
 
 def post_share(request, post_id):
@@ -61,3 +64,15 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, "blog/post/share.html", {"post": post, "form": form, "sent":sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/comment.html", {"post":post, "form":form, "comment":comment})
